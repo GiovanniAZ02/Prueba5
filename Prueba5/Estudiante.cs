233e0248a7b59243deb392dba4860efbc81c8803
Giovanni <alemangiovanni51@gmail.com>
2024-10-06 16:27:10 -0600

    Se crea el método “Extensiones” dentro de este metodo se hara otro estatico "Mostrar Informacion" donde nos imprimirá los datos de los estudiantes que se lleguen a ingresar y si cumplen con el requisito(75%) gracias a la condicional “if”.

---
+++ Prueba5/Estudiante.cs
@@ -0,0 +1,6 @@
+﻿namespace Tarea
+{
+    public class Estudiante
+    {
+    }
+}
--- Prueba5/Universidad.cs
+++ Prueba5/Universidad.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using static Prueba5.Universidad;
+using static Prueba5.Universidad.Estudiantes;
 
 namespace Prueba5
 {
@@ -24,6 +27,27 @@ namespace Prueba5
                     }
                     return (asistencias / totalSesiones) * 100;
                 }
+            }
+        }
+    }
+
+    public static class Extensiones
+    {
+        public static void MostrarInformacion(this Estudiantes texto)
+        {
+            double asistencia = Asistencia.Porcentaje(texto.TotalAsist, texto.TotalSec);
+            Console.WriteLine($"Nombre del Estudiante: {texto.Nombre}");
+            Console.WriteLine($"Total de Sesiones: {texto.TotalSec}");
+            Console.WriteLine($"Sesiones Asistidas: {texto.TotalAsist}");
+            Console.WriteLine($"Porcentaje de Asistencia: {asistencia}%");
+
+            if (asistencia >= 75)
+            {
+                Console.WriteLine("El estudiante cumple con el requisito de asistencia.");
+            }
+            else
+            {
+                Console.WriteLine("El estudiante NO cumple con el requisito de asistencia.");
             }
         }
     }
