2a91b171832a11fd9f45305baf991e41780374db
Giovanni <alemangiovanni51@gmail.com>
2024-10-06 15:21:32 -0600

    Se crea la clase publica Universidad y una clase anidada llamada estudiantes con sus propiedades luego la clase interna Asistencia con una clase estatica donde se calcula las asistencias, sesiones y retornara el resultado.

--- Prueba5/Program.cs
+++ Prueba5/Program.cs
@@ -4,7 +4,7 @@ namespace Prueba5
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
         }
---
+++ Prueba5/Universidad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba5
+{
+    public class Universidad
+    {
+        public class Estudiantes
+        {
+            public string Nombre { get; set; }
+            public double TotalSec { get; set; } // Total de sesiones
+            public double TotalAsist { get; set; } // Asistencias
+
+            public class Asistencia
+            {
+                public static double Porcentaje(double asistencias, double totalSesiones)
+                {
+                    if (totalSesiones == 0)
+                    {
+                        return 0; // Evitar división por cero
+                    }
+                    return (asistencias / totalSesiones) * 100;
+                }
+            }
+        }
+    }
+}
