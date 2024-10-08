f0969e2586790ef7bab289d9c418394b7297fe93
GiovanniAZ02 <AlemanZ1998@gmail.com>
2024-10-07 18:47:47 -0600

    En esta clase iniciamos con program y el metodo Main dentro de este definimos variables, el grupo como detalle y un bucle para validar los datos ingresados seguido unos arreglos para almacenar los datos de los estudiantes

--- Prueba5/Program.cs
+++ Prueba5/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace Prueba5
+namespace Tarea
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            int CalculoTotal = 0;
 
+            Console.WriteLine("Grupo 1M2-S");
+
+            do
+            {
+                Console.WriteLine("Ingrese la Asistencia Total: ");
+                CalculoTotal = Convert.ToInt32(Console.ReadLine());
+                if (CalculoTotal < 0)
+                {
+                    Console.WriteLine("Ingrese un numero Positivo");
+                }
+
+            } while (CalculoTotal < 0);
+
+
+            Console.WriteLine("Ingrese la Cantidad de Estudiante");
+            int estudiante = Convert.ToInt32(Console.ReadLine());
+
+            string[] CantidadNombre = new string[estudiante];
+            double[] CantidadAsis = new double[estudiante];
         }
     }
 }
